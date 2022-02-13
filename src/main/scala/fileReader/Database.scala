99b2041d55f2d8abce19cfe9ee288f7431403a09
Quentin Felten <quentin.felten@efrei.net>
2022-02-13 14:56:04 +0100

    comment DB

--- src/main/scala/fileReader/Database.scala
+++ src/main/scala/fileReader/Database.scala
@@ -1,6 +1,6 @@
 import scala.slick.driver.MySQLDriver.simple._
 import scala.slick.jdbc.meta._
-
+/*
 sealed class Database(){
 
     val db = Database.forURL(
@@ -57,3 +57,4 @@ sealed class Database(){
 
     
 }
+*/
